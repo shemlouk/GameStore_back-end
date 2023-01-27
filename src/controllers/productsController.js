95d23f4f3f5102b42c69797321cfd830e194ca4e
samuel <shemlouk@gmail.com>
2023-01-26 21:35:17 -0300

    fix(controllers): turn create function from productsController into async function

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,7 +1,7 @@
 import { productSchema } from "../schemas/index.js";
 import { PRODUCTS } from "../database.js";
 
-export const create = (req, res) => {
+export const create = async (req, res) => {
   const { error } = productSchema.validate(req.body, { abortEarly: false });
   if (error) return res.status(422).json(error.details);
   const newProduct = req.body;
