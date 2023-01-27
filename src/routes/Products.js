dbeecb0c446e788b7921fbdb18a97ae41d3b8a96
samuel <shemlouk@gmail.com>
2023-01-26 23:29:56 -0300

    feat(routes): add authentication middleware to Products GET endpoint

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,9 +1,10 @@
 import { create, read } from "../controllers/productsController.js";
+import authentication from "../middlewares/authentication.js";
 import { Router } from "express";
 
 const route = Router();
 
 route.post("/products", create);
-route.get("/products", read);
+route.get("/products", authentication, read);
 
 export default route;
