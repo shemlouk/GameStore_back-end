2e49ae52fbb2ec6d00613f766f67d7f54be8a119
samuel <shemlouk@gmail.com>
2023-01-26 21:36:31 -0300

    feat(routes): add GET endpoint to Products route

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,8 +1,9 @@
-import { create } from "../controllers/productsController.js";
+import { create, read } from "../controllers/productsController.js";
 import { Router } from "express";
 
 const route = Router();
 
 route.post("/products", create);
+route.get("/products", read);
 
 export default route;
