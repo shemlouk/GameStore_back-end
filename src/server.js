86b10fb4bf75e64c790a2f75caaab56f91ec9468
samuel <shemlouk@gmail.com>
2023-01-29 19:48:57 -0300

    refactor(users): add Users route to app

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import Products from "./routes/Products.js";
-import { cadastro, login, updateImg } from "./routes.js";
+import Users from "./routes/Users.js";
 
 dotenv.config();
 
@@ -11,10 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(Products);
-
-app.post(`/sign-up`, cadastro)
-app.post(`/login`, login)
-app.post(`/update-img`, updateImg)
+app.use(Users);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running");
