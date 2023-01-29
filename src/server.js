14b4cc2105cd80df9c7d0679a0ad9755f2f91a3b
Davi-Mancebo <davimancebo@gmail.com>
2023-01-29 09:29:07 -0300

    update remove server

--- src/server.js
+++ src/server.js
@@ -10,9 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post(`${process.env.MONGO_URI}/sign-up`, cadastro)
-app.post(`${process.env.MONGO_URI}/login`, login)
-app.post(`${process.env.MONGO_URI}/update-img`, updateImg)
+app.post(`/sign-up`, cadastro)
+app.post(`/login`, login)
+app.post(`/update-img`, updateImg)
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running");
