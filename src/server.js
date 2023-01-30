96509e1723ef6cdebec616bf761f73bbb11f002d
samuel <shemlouk@gmail.com>
2023-01-30 00:36:30 -0300

    feat(cart): add cart route to app

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import Products from "./routes/Products.js";
 import Users from "./routes/Users.js";
+import Cart from "./routes/Cart.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cors());
 app.use(Products);
 app.use(Users);
+app.use(Cart);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running");
