51c1fb45e29e7d5d41a8fe70f840d3cfcfdb634a
samuel <shemlouk@gmail.com>
2023-01-30 15:42:32 -0300

    feat: add total increment function

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,14 +1,20 @@
-import { CARTS } from "../database.js";
+import { CARTS, PRODUCTS } from "../database.js";
+import { ObjectId } from "mongodb";
 
 export const add = async (req, res) => {
   const { id } = req.body;
   const userId = res.locals.userId;
   try {
+    const { price } = await PRODUCTS.findOne({ _id: ObjectId(id) });
+    if (!price) return res.sendStiatus(422);
     const duplicate = await CARTS.findOne({
       $and: [{ userId }, { products: { $in: [id] } }],
     });
     if (duplicate) return res.sendStatus(409);
-    CARTS.updateOne({ userId }, { $push: { products: id } });
+    await CARTS.updateOne(
+      { userId },
+      { $push: { products: id }, $inc: { total: price } }
+    );
     res.sendStatus(200);
   } catch (err) {
     res.status(500).json(err);
@@ -18,7 +24,12 @@ export const add = async (req, res) => {
 export const remove = async (req, res) => {
   const { id } = req.body;
   try {
-    CARTS.updateOne({ userId: res.locals.userId }, { $pull: { products: id } });
+    const { price } = await PRODUCTS.findOne({ _id: ObjectId(id) });
+    if (!price) return res.sendStiatus(422);
+    CARTS.updateOne(
+      { userId: res.locals.userId },
+      { $pull: { products: id }, $inc: { total: -price } }
+    );
     res.sendStatus(200);
   } catch (err) {
     res.status(500).json(err);
@@ -27,7 +38,10 @@ export const remove = async (req, res) => {
 
 export const empty = async (req, res) => {
   try {
-    CARTS.updateOne({ userId: res.locals.userId }, { $set: { products: [] } });
+    CARTS.updateOne(
+      { userId: res.locals.userId },
+      { $set: { products: [], total: 0 } }
+    );
     res.sendStatus(200);
   } catch (err) {
     res.status(500).json(err);
@@ -36,8 +50,10 @@ export const empty = async (req, res) => {
 
 export const read = async (req, res) => {
   try {
-    const { products } = await CARTS.findOne({ userId: res.locals.userId });
-    res.send(products);
+    const { products, total } = await CARTS.findOne({
+      userId: res.locals.userId,
+    });
+    res.send({ products, total });
   } catch (err) {
     res.status(500).json(err);
   }
